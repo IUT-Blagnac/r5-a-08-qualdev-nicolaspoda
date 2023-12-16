dad309c98528a485f39a426491e16ed819c4d63b
nicolaspoda <nicolaspoda@gmail.com>
2023-12-16 13:31:13 +0100

    tp4

--- cucumber_demo/src/test/java/dojo/RunCucumberTest.java
+++ cucumber_demo/src/test/java/dojo/RunCucumberTest.java
@@ -1,7 +1,19 @@
-import cucumber.api.junit.Cucumber;
-import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+/*
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+*/
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/dojo/resources", plugin = {"json:target/cucumber.json"})
 public class RunCucumberTest {
 }
+
+/*
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/java/dojo/resources", plugin = {"json:target/cucumber.json"})
+public class RunCucumberTest {
+}
+*/
